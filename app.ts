454617708ca06cd4dda61988521fb0245f176af1
AryaD14 <aryadanech9@gmail.com>
2023-07-13 09:26:16 +0530

    explicit types in typescript

--- app.ts
+++ app.ts
@@ -40,3 +40,68 @@ newObj = {
     pass: true
 }; // if we remove any one of the property from this updated object, then we will get an error because the structure of the updated object should be same as the object initially declared.
 // the values can be changed but the datatypes should remain same for the existing object
+
+/* ----------------------------------------------------------------------------------------------------------------------------- */
+
+/** Explicit Types */
+//----------------------------------------------------
+
+let username: string;
+let userAge: number;
+let userActive: boolean;
+//we can use this method to initialize a variable without actually giving it a value
+
+username = 'Arya';
+userAge = 10;
+userActive = false;
+//now while giving the variables a value, we need to match the data types provided to them when initializing
+
+
+/** Arrays */
+//-----------------------------------------------------------------------
+
+let userArray: string[] = []; //initializes empty array for values of string data type only
+// we cannot directly push a value in the array as it is not declared it as an empty array
+
+userArray.push('JavaScript');
+userArray = ['Arya', 'Hello', 'Position', 'TypeScript'];
+// userArray.push(50); (not allowed as userArray is defined to contain only string values)
+
+
+/** Mixed Types */
+// ---------------------------------------------------------------------------
+
+let mixArr: (string | number | boolean)[] = []; // it tells that an array can contain values for string or number or boolean 
+mixArr.push(21);
+mixArr.push("Arya");
+mixArr.push(true);
+mixArr.push("Krish");
+console.log(mixArr);
+
+
+//now to declared a variable to contain string or number value
+let userID: string | number; // now variable userID can contain string or a numerical value
+
+/** Objects */
+// ---------------------------------------------------------------------------
+
+let objectOne: object;
+objectOne = {
+    name: "Arya",
+    age: 21,
+    isPass: true
+}; //as variable objectOne is given the type of object, it can store value for any data type without following a structure of variable
+
+let objectTwo: {
+    name: string,
+    age: number,
+    isPass: boolean
+}; // now when declaring values to objectTwo, we need to declare value for all the properties like name, age, and isPass
+
+objectTwo = {
+    name: 'Krish',
+    age: 20,
+    isPass: false
+};
+
+console.log(objectTwo)
